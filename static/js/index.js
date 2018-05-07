078ddd519cf32a89834a0ca670d8b391ce841e9e
Hugo Diniz <hhldiniz@gmail.com>
2018-05-07 19:19:32 -0300

    validação para o formulário de estados e temas do noty

--- static/js/index.js
+++ static/js/index.js
@@ -3,24 +3,62 @@ $(document).ready(function(){
         let state_name = $("#state_name").val();
         let pib = $("#pib").val();
         let population = $("#population").val();
-        let xmlHttpRequest = new XMLHttpRequest();
-        xmlHttpRequest.onreadystatechange = function(){
-            if(this.readyState === 4 && this.status === 200)
-            {
-                let response = JSON.parse(this.responseText);
-                if(response['result'])
-                    new Noty({
-                        text: "Cadastro realizado com sucesso!",
-                        type: "success"
-                    }).show();
-                else
-                    new Noty({
-                        text:"Um erro ocorreu ao realizar a inclusão do estado",
-                        type:"error"
-                    }).show();
-            }
-        };
-       xmlHttpRequest.open("POST","/", true);
-       xmlHttpRequest.send(`{"state_name": "${state_name}", "pib": "${pib}", "population": "${population}"}`);
+        let data = {"state_name": state_name, "pib": pib, "population": population};
+        if(validateStateFormFields(data))
+        {
+                let xmlHttpRequest = new XMLHttpRequest();
+                xmlHttpRequest.onreadystatechange = function(){
+                    if(this.readyState === 4 && this.status === 200)
+                    {
+                        let response = JSON.parse(this.responseText);
+                        if(response['result'])
+                            new Noty({
+                                text: "Cadastro realizado com sucesso!",
+                                type: "success",
+                                theme: "relax"
+                            }).show();
+                        else
+                            new Noty({
+                                text:"Um erro ocorreu ao realizar a inclusão do estado",
+                                type:"error",
+                                theme: "relax"
+                            }).show();
+                    }
+                };
+               xmlHttpRequest.open("POST","/", true);
+               xmlHttpRequest.send(JSON.stringify(data));
+        }
+        else
+            new Noty({
+               text: "Você forneceu dados inválidos",
+               type: "error",
+               theme: "relax"
+            }).show();
     });
+
+    function validateStateFormFields(data) {
+        let regex = /\d+/;
+        for(let obj in data)
+        {
+            if(data.hasOwnProperty(obj))
+            {
+               switch (obj)
+               {
+                   case "state_name":
+                       if(data[obj].length === 0)
+                           return false;
+                       break;
+                   case "pib":
+                       if(data[obj] === "0" || data[obj] === undefined || data[obj].length === 0 || !regex.test(data[obj]))
+                           return false;
+                       break;
+                   case "population":
+                        if(data[obj] === "0" || data[obj] === undefined || data[obj].length === 0 || !regex.test(data[obj]))
+                           return false;
+                        break;
+               }
+            }
+        }
+        return true;
+    }
 });
